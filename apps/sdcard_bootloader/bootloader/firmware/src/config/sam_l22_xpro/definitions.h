261d6855dd0958e757beb51144b7eeaa6b8e0af6
Amit Raddi <amit.raddi@microchip.com>
2021-04-11 10:43:20 +0530

    [SDCARD] Regenerated sdcard_bootloader_sam_l22_xpro with latest file system bootloader component
    MH3-51879
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/sdcard_bootloader/bootloader/firmware/src/config/sam_l22_xpro/definitions.h
+++ apps/sdcard_bootloader/bootloader/firmware/src/config/sam_l22_xpro/definitions.h
@@ -48,18 +48,18 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
-#include "peripheral/sercom/spim/plib_sercom3_spi.h"
+#include "peripheral/sercom/spi_master/plib_sercom3_spi_master.h"
 #include "peripheral/nvmctrl/plib_nvmctrl.h"
 #include "peripheral/evsys/plib_evsys.h"
+#include "bootloader/bootloader.h"
+#include "peripheral/pm/plib_pm.h"
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/dmac/plib_dmac.h"
-#include "peripheral/pm/plib_pm.h"
 #include "peripheral/tc/plib_tc0.h"
 #include "driver/sdspi/drv_sdspi.h"
 #include "system/time/sys_time.h"
-#include "bootloader/bootloader.h"
 #include "system/fs/sys_fs.h"
 #include "system/fs/sys_fs_media_manager.h"
 #include "system/fs/sys_fs_fat_interface.h"
@@ -83,6 +83,9 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* CPU clock frequency */
+#define CPU_CLOCK_FREQUENCY 32000000
 
 // *****************************************************************************
 // *****************************************************************************
@@ -176,22 +179,22 @@ void SYS_Tasks ( void );
 // Section: Type Definitions
 // *****************************************************************************
 // *****************************************************************************
-    
+
 // *****************************************************************************
 /* System Objects
-        
+
 Summary:
     Structure holding the system's object handles
-        
+
 Description:
     This structure contains the object handles for all objects in the
     MPLAB Harmony project's system configuration.
-        
+
 Remarks:
     These handles are returned from the "Initialize" functions for each module
     and must be passed into the "Tasks" function for each module.
 */
-        
+
 typedef struct
 {
     /* SDSPI0 Driver Object */
